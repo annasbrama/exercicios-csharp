536ce1e281f975da20d7bc75eaef5d00ea04a6ec
Anna <acesbrama@gmail.com>
2025-08-31 17:57:02 -0300

    feat: Exercicio - Sinal

--- Program.cs
+++ Program.cs
@@ -1,7 +1,23 @@
-﻿//Leia um número inteiro e exiba a mensagem "Você digitou um número negativo." caso o número seja menor que zero.
+﻿// Leia um número inteiro e exiba a mensagem "Você digitou um número negativo." caso o número seja menor que zero.
+// Console.WriteLine("Digite um número");
+// int num1 = int.Parse(Console.ReadLine());
+// if (num1 < 0)
+// {
+//     Console.WriteLine("Você digitou um número negativo");
+// }
+
+// Leia um número inteiro e exiba "Negativo", "Zero" ou "Positivo", conforme o caso.
 Console.WriteLine("Digite um número");
-int num1 = int.Parse(Console.ReadLine());
-if (num1 < 0)
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (num1 > 0)
 {
-    Console.WriteLine("Você digitou um número negativo");
+    Console.WriteLine("Positivo");
+}
+else if (num1 == 0)
+{
+    Console.WriteLine("Zero");
+}
+else
+{
+    Console.WriteLine("Negativo");
 }
