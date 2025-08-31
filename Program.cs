86ed3565dcd79c341e6410a24f74a9a4b074fec8
Anna <acesbrama@gmail.com>
2025-08-31 17:52:44 -0300

    feat: Exercício -  Negativo

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿//Leia um número inteiro e exiba a mensagem "Você digitou um número negativo." caso o número seja menor que zero.
+Console.WriteLine("Digite um número");
+int num1 = int.Parse(Console.ReadLine());
+if (num1 < 0)
+{
+    Console.WriteLine("Você digitou um número negativo");
+}
